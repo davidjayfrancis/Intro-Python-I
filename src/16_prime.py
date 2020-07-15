a81297b3d49342cd20baaa9b5e08f36aa30df1cc
David Francis <davidjayfrancis@gmail.com>
2020-07-14 21:48:39 -0700

    refactored solution and added edge cases

--- src/16_prime.py
+++ src/16_prime.py
@@ -1,27 +1,32 @@
 import math
 
 def primeChecker(num):
-    if num % 2 == 0:
+    if num == 1:
         return False
-    if num % 3 == 0:
-        return False
-
-    n = 5
-    for i in range(5,math.ceil(math.sqrt(num))):
-        if num % i == 0:
-            print(f"{num} is not prime. {num} is divisible by {i}.")
+    elif num == 2:
+        return True
+    elif num == 3:
+        return True
+    else:
+        if num % 2 == 0 or num % 3 == 0:
             return False
-    
-    return True
+
+        n = 5
+        for i in range(5,math.ceil(math.sqrt(num))):
+            if num % i == 0:
+                return False
+        return True
+        # add every prime multiple while the prime * 2 < num
 
 
-    
-        
-            
-
-    return nums
-    # add every prime multiple while the prime * 2 < num
-
-
-print(primeChecker(100))
-print(primeChecker(95))
+print(primeChecker(1))
+print(primeChecker(2))
+print(primeChecker(3))
+print(primeChecker(4))
+print(primeChecker(5))
+print(primeChecker(6))
+print(primeChecker(7))
+print(primeChecker(21))
+print(primeChecker(29))
+print(primeChecker(97))
+print(primeChecker(52363))
